0fec411600ff3328fc527cdea12b12627a8f3836
izio38 <romainbillot3009@gmail.com>
2019-12-02 20:43:56 +0100

    feat( controller/sector: create and update are in a working state )

--- controller/sector.php
+++ controller/sector.php
@@ -1,26 +1,45 @@
 <?php
 
 use model\manager\SectorManager;
+use model\Sector;
 
 require_once "./model/manager/sectorManager.php";
 
 if (isset($_GET)) {
     switch ($action) {
+        case 'handle_submit':
+
+            // Retrieve the id if it's an edit, else it will be null.
+            $id = null;
+            if (isset($_POST["id"])) {
+                $id = intval($_POST["id"]);
+            }
+
+            // Try to create the entity with the constructor.
+            $sector = new Sector($id, $_POST["label"]);
+            SectorManager::save($sector);
+
+            $view = "sector/list";
+            $sectors = SectorManager::getAll();
+            break;
         case 'new':
             $sector = null;
             $view = "sector/form";
+
             break;
         case 'delete':
             $id = $_GET['id'];
 
             // @TODO: Check if there is no attached structure to this sector.
+
+            $view = "sector/list";
+            $sectors = SectorManager::getAll();
             break;
         case 'edit':
 
             $view = "sector/form";
             $id = $_GET['id'];
             $sector = SectorManager::getFromId($id);
-
             break;
         case 'list':
             $view = "sector/list";
